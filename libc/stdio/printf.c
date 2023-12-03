353aaa35f31e0d9c20f070b518ae20b5f73f7623
Toto <totooodev@gmail.com>
2023-12-03 12:23:13 +0100

    Rewrote printf to allow for %d and %x parameters

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -14,77 +14,116 @@ static bool print(const char* data, size_t length) {
     return true;
 }
 
-int printf(const char* formatted_str, ...) {
+int int_to_string_decimal(char* str, int number) {
+    int length = 0;
+
+    if (number < 0) {
+        str[length] = '-';
+        number = -number;
+        length++;
+    }
+
+    while (number != 0) {
+        int digit = number % 10;
+        str[length] = digit + '0';
+        number /= 10;
+        length++;
+    }
+
+    str[length] = '\0';
+
+    int start = 0;
+    int end = length - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    return length;
+}
+
+int int_to_string_hex(char* str, int number) {
+    int length = 0;
+
+    char hex_digits[16] = "0123456789abcdef";
+
+    while (number != 0) {
+        int digit = number % 16;
+        str[length] = hex_digits[digit];
+        number /= 16;
+        length++;
+    }
+
+    str[length] = '\0';
+
+    int start = 0;
+    int end = length - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    return length;
+}
+
+int printf(const char* format, ...) {
     va_list parameters;
-    va_start(parameters, formatted_str);
+    va_start(parameters, format);
 
-    int written = 0;
+    int written_chars = 0;
 
-    while (*formatted_str != '\0') {
-        size_t maxrem = INT_MAX - written;
-
-        if (formatted_str[0] != '%' || formatted_str[1] == '%') {
-            if (formatted_str[0] == '%')
-                formatted_str++;
-
-            size_t amount = 1;
-            while (formatted_str[amount] && formatted_str[amount] != '%')
-                amount++;
-
-            if (amount > maxrem)
-                return -1;
-
-            if (!print(formatted_str, amount))
-                return -1;
-
-            formatted_str += amount;
-            written += amount;
-            continue;
-        }
-        
-        const char* format_begun_at = formatted_str;
-        formatted_str++;
-
-        if (*formatted_str == 'c') {
-            formatted_str++;
-            char c = (char)va_arg(parameters, int); // char promotes to int
-
-            if (!maxrem)
-                return -1;
-
-            if (!printf(&c, sizeof(c)))
-                return -1;
-
-            written++;
-        }
-        else if (*formatted_str == 's') {
-            formatted_str++;
-            const char* str = (const char*)va_arg(parameters, const char*);
-            size_t len = strlen(str);
-
-            if (!maxrem)
-                return -1;
-
-            if (!printf(str, len))
-                return -1;
-
-            written += len;
+    while (format[0] != '\0') {
+        if (format[0] != '%') {
+            putchar(format[0]);
+            written_chars++;
+            format++;
         }
         else {
-            formatted_str = format_begun_at;
-			size_t len = strlen(formatted_str);
+            format++;
+            
+            if (format[0] == 's') {
+                const char* param = (const char*)va_arg(parameters, const char*);
+                int length = strlen(param);
+                print(param, length);
+                written_chars += length;
+            }
+            else if (format[0] == 'c') {
+                char param = (char)va_arg(parameters, int);
+                putchar(param);
+                written_chars++;
+            }
+            else if (format[0] == 'd') {
+                int param = (int)va_arg(parameters, int);
+                char str[7];
+                int length = int_to_string_decimal(str, param);
+                print(str, length);
+                written_chars += length;
+            }
+            else if (format[0] == 'x') {
+                int param = (int)va_arg(parameters, int);
+                char str[9];
+                int length = int_to_string_hex(str, param);
+                print(str, length);
+                written_chars += length;
+            }
+            else {
+                putchar('%');
+                putchar(format[0]);
+                written_chars += 2;
+            }
 
-			if (maxrem < len)
-				return -1;
-
-			if (!print(formatted_str, len))
-				return -1;
-			
-            written += len;
-			formatted_str += len;
+            format++;
         }
     }
 
     va_end(parameters);
-    return written;
+    return written_chars;
 }
