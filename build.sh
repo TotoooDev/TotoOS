61b946be2d5cb5f7b63375963552a42df100253f
toto <tom.mandin@etu.univ-poitiers.fr>
2024-05-05 10:21:56 +0200

    fixed build not creating the iso directory

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -T linker.ld -o toto_os.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 
 ./check-multiboot.sh
+mkdir -p iso/boot/grub
 cp toto_os.bin iso/boot/toto_os.bin
 cp grub.cfg iso/boot/grub/grub.cfg
 grub-mkrescue -o toto_os.iso iso
